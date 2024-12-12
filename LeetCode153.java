2ccec628fc95e4ec8561cdc97432ebf097bf6d2b
SOLAI SAKTHI DASAN S <112788138+solai-sakthi-dasan-23@users.noreply.github.com>
2024-12-12 12:30:27 +0530

    LeetCode 153- Find Minimum in Rotated Sorted Array

---
+++ LeetCode153.java
@@ -0,0 +1,56 @@
+package dsa;
+
+class LeetCode153 {
+
+    // 153. Find Minimum in Rotated Sorted Array
+
+    /*Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+    For example, the array nums = [0,1,2,4,5,6,7] might become:
+        [4,5,6,7,0,1,2] if it was rotated 4 times.
+        [0,1,2,4,5,6,7] if it was rotated 7 times.
+    Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+    Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+    You must write an algorithm that runs in O(log n) time */
+
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        /*Example 1:
+        Input: nums = [3,4,5,1,2]
+        Output: 1
+        Explanation: The original array was [1,2,3,4,5] rotated 3 times.*/
+
+        int example1 = findMin(new int[]{3,4,5,1,2});
+        System.out.println(example1);
+
+        /*Example 2:
+        Input: nums = [4,5,6,7,0,1,2]
+        Output: 0
+        Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.*/
+
+        int example2 = findMin(new int[]{4,5,6,7,0,1,2});
+        System.out.println(example2);
+
+        /*Example 3:
+        Input: nums = [11,13,15,17]
+        Output: 11
+        Explanation: The original array was [11,13,15,17] and it was rotated 4 times.*/
+
+        int example3 = findMin(new int[]{11,13,15,17});
+        System.out.println(example3);
+    }
+}
